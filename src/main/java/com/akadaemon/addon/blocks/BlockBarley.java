dc27deb93ea6d8f355cd47041d4c31e3ba64a1ee
Aleksey Schumann <aleksey.schumann@gmail.com>
2026-03-11 19:06:34 +0300

    fix barley

--- src/main/java/com/akadaemon/addon/blocks/BlockBarley.java
+++ src/main/java/com/akadaemon/addon/blocks/BlockBarley.java
@@ -5,8 +5,11 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 
 public class BlockBarley extends BlockCrops {
     @SideOnly(Side.CLIENT)
@@ -25,6 +28,52 @@ public class BlockBarley extends BlockCrops {
     @Override
     protected Item func_149865_P() {
         return AkadaemonAddon.barleySeeds;
+    }
+
+    @Override
+    public java.util.ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
+        java.util.ArrayList<ItemStack> ret = new java.util.ArrayList<ItemStack>();
+
+        if (metadata >= 5) {
+            ret.add(new ItemStack(AkadaemonAddon.barley, 1 + world.rand.nextInt(2)));
+            ret.add(new ItemStack(AkadaemonAddon.barleySeeds, 1 + world.rand.nextInt(3)));
+        } else {
+            ret.add(new ItemStack(AkadaemonAddon.barleySeeds, 1));
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+        int meta = world.getBlockMetadata(x, y, z);
+
+        if (meta >= 5) {
+            if (!world.isRemote) {
+                java.util.ArrayList<ItemStack> items = this.getDrops(world, x, y, z, meta, 0);
+                boolean seedUsed = false;
+
+                for (ItemStack stack : items) {
+                    if (stack == null || stack.stackSize <= 0) continue;
+
+                    if (!seedUsed && stack.getItem() == AkadaemonAddon.barleySeeds) {
+                        stack.stackSize--;
+                        seedUsed = true;
+                    }
+
+                    if (stack.stackSize > 0) {
+                        net.minecraft.entity.item.EntityItem entityItem = new net.minecraft.entity.item.EntityItem(
+                                world, x + 0.5D, y + 0.1D, z + 0.5D, stack
+                        );
+                        world.spawnEntityInWorld(entityItem);
+                    }
+                }
+                world.setBlockMetadataWithNotify(x, y, z, 0, 2);
+                world.playSoundEffect(x + 0.5D, y + 0.5D, z + 0.5D, "dig.grass", 0.7F, 0.9F);
+            }
+            player.swingItem();
+            return true;
+        }
+        return false;
     }
 
     @Override
